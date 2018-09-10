2640c4ad9d88bdd4456eef20c5f6b2f9b0e8bf02
Konstantin <ignatjev.konstantin@gmail.com>
2018-09-10 23:12:53 +0300

    rename folder

---
+++ Array_practicum .playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var arrayOne = [1, 2, 3, 4, 5]
+var arrayTwo: [String] = ["one", "two", "three", "four"]
+var arrayThree = Array(1...9)
+
+arrayTwo.append("toy")
+
+
+
+
